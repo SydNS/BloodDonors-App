3cd1910dcf04abedbea4c24e560f59529c92f5d8
SydNS <sydneynsiimbe@gmail.com>
2022-01-22 17:53:04 +0300

    added a thread to make a splash for after a second

--- app/src/main/java/com/example/blooddonationapp/MainActivity.kt
+++ app/src/main/java/com/example/blooddonationapp/MainActivity.kt
@@ -1,16 +1,38 @@
 package com.example.blooddonationapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
+import android.view.WindowManager
 import android.widget.RadioButton
 import android.widget.RadioGroup
 import android.widget.Toast
+import com.example.blooddonationapp.authpages.Login
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)}
+        setContentView(R.layout.activity_main)
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+
+        Handler().postDelayed({
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+            finish()
+        }, 1000) // 3000 is the delayed time in milliseconds.
+
+
+
+    }
 
 }
